15b63b85b8c264b2f3b3d80bd24bc2811ea5f4d8
Xyber Pastoril <cyboryan@gmail.com>
2021-08-10 09:45:29 +0800

    Implement change password

---
+++ app/Rules/MatchOldPassword.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class MatchOldPassword implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return \Illuminate\Support\Facades\Hash::check($value, auth()->user()->password);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute is match with old password.';
+    }
+}
