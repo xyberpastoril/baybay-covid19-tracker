407a6265a25f27e85860e6c2330c14df37ea41c7
Xyber Pastoril <cyboryan@gmail.com>
2021-08-10 10:44:57 +0800

    fix can't create activecases log entry

--- app/Models/ActiveCases.php
+++ app/Models/ActiveCases.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActiveCases extends Model 
 {
-
+    protected $primaryKey = null;
+    public $incrementing = false;
     protected $table = 'active_cases_log';
     public $timestamps = true;
     protected $fillable = array('date_issued', 'confirmed', 'probable', 'suspected', 'reference');
